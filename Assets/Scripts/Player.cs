41ebca41b39ae1504a8ad1e7a96e94d8d9230d1e
Candidio <choinski.czukczuk@gmail.com>
2018-11-14 22:27:11 +0100

    Stworzenie mechaniki tury
    Rozbudowałem metodę końca tury o zmianę nie tylko gracza, ale i tury, fazy, dnia i tygodnia. Na początku gry losuje listy zakupów dla poszczególnych graczy.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player {
+
+    GameObject[] pawns = new GameObject[5];
+    List<GameObject> pawnsInHand = new List<GameObject>();
+    public Color pawnColor;
+
+    public int numberOfPlayer;
+    public bool isPlayerAI;
+    public ShoppingList shoppinglist;
+
+    public Player(Color color, int numberOfPlayer, bool isPlayerAI, ShoppingList shoppinglist)
+    {
+        this.pawnColor = color;
+        this.numberOfPlayer = numberOfPlayer;
+        this.isPlayerAI = isPlayerAI;
+        this.shoppinglist = shoppinglist;
+    }
+
+    public void MakeMove ()
+    {
+
+    }
+}
