6372a9975ce9a9caecd730a278d7d1490952bb3a
Damian Białobrzycki <wilkzklatki@gmail.com>
2018-11-17 19:29:55 +0100

    Dodano tekst UI z postępem gry.
    UI wyświetla aktualny dzień, fazę gry, gracza który obecnie gra i przygotowano wstępny kod na dopięcie zmiany listy zakupów.

--- Assets/Scripts/ShoppingList.cs
+++ Assets/Scripts/ShoppingList.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShoppingList : MonoBehaviour {
 
+    public Sprite image;
     [SerializeField] string target;
     [SerializeField] int electronic;
     [SerializeField] int grocery;
@@ -11,8 +13,9 @@ public class ShoppingList : MonoBehaviour {
     [SerializeField] int cloth;
     [SerializeField] int furniture;
 
-    public ShoppingList (string target, int electronic, int grocery, int newsman, int cloth, int furniture)
+    public ShoppingList (Sprite image, string target, int electronic, int grocery, int newsman, int cloth, int furniture)
     {
+        this.image = image;
         this.target = target;
         this.electronic = electronic;
         this.grocery = grocery;
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Text dayText;
+    public Text weekText;
+    public Text playerText;
+    public Text phaseText;
+    public Image shoppingList;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateDay(GameManager.Day day)
+    {
+        dayText.text = "Day: " + day.ToString();
+    }
+
+    public void UpdateWeek(int week)
+    {
+        weekText.text = "Week: " + week;
+    }
+
+    public void UpdatePlayer(int player)
+    {
+        playerText.text = "Now playing: Player " + (player + 1);
+    }
+
+    public void UpdatePhase(GameManager.Phase phase)
+    {
+        phaseText.text = "Phase: " + phase.ToString();
+    }
+
+    public void UpdateShoppingList(Sprite image)
+    {
+        shoppingList.sprite = image;
+    }
+}
